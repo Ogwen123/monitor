ff53e0345865229f00752e4d1e5fa0a2dc1593bf
Owen Jones <owen.d.jones@btinternet.com>
2025-06-08 21:54:49 +0100

    path fixing attempt 6

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use actix_web::{get, App, HttpResponse, HttpServer, Error};
 use actix_files::NamedFile;
 use std::path::PathBuf;
 use std::env::current_exe;
-use std::time;
-use async_std::task;
 use sysinfo::{Disks, System};
 use serde::Serialize;
 use crate::utils::logger::{fatal};
@@ -73,13 +71,14 @@ fn get_dir_path() -> String {
 
 #[get("/")]
 async fn index() -> actix_web::Result<NamedFile> {
-    let mut path: PathBuf = PathBuf::from(get_dir_path() + "index.html");
+    let path: PathBuf = PathBuf::from(get_dir_path() + "index.html");
+    println!("{}", get_dir_path() + "index.css");
     Ok(NamedFile::open(path)?)
 }
 
 #[get("/index.css")] // why do I have to do this 💀
 async fn css() -> actix_web::Result<NamedFile> {
-    let mut path: PathBuf = PathBuf::from(get_dir_path() + "index.css");
+    let path: PathBuf = PathBuf::from(get_dir_path() + "index.css");
     Ok(NamedFile::open(path)?)
 }
 
